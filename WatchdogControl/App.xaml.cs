d3c5110e0980f0e0a5433e49cc1e0915eef6a1cd
EDogaev <edogaev@yandex.ru>
2025-08-28 10:17:15 +0300

    Добавление Host

--- WatchdogControl/App.xaml.cs
+++ WatchdogControl/App.xaml.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System.Windows;
+using WatchdogControl.Interfaces;
+using WatchdogControl.Models.MemoryLog;
+using WatchdogControl.Models.Watchdog;
 using WatchdogControl.Services;
+using WatchdogControl.ViewModels;
+using WatchdogControl.Views;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace WatchdogControl
 {
@@ -11,42 +19,97 @@ namespace WatchdogControl
     public partial class App : Application
     {
         /// <summary> Переменная, указывающая, что приложение находится в Design-time </summary>
-        public static bool IsDesignMode { get; private set; } = true;
+        public static bool IsDesignMode { get; } = Application.Current is not App;
 
-        private readonly ILogger _logger = Bootstrapper.Container.GetRequiredService<ILogger<App>>();
+        private IHost _host;
+        private ILogger _logger;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             AppService.OnStartup();
+            base.OnStartup(e);
 
-            // выходим из Design-time (переходим в Run-time)
-            IsDesignMode = false;
+            CreateHost();
+
+            _logger = _host.Services.GetRequiredService<ILogger<App>>();
 
             _logger.LogInformation("Запуск приложения");
-
-            Bootstrapper.RunApp();
 
             // перехват необработанных ошибок
             AppDomain.CurrentDomain.UnhandledException += (_, e) =>
             {
                 var ex = (Exception)e.ExceptionObject;
-                _logger.LogInformation($"Ошибка: {ex.Message}");
+                _logger.LogError($"Ошибка: {ex.Message}");
             };
 
             TaskScheduler.UnobservedTaskException += (_, e) =>
             {
-                _logger.LogInformation($"Ошибка: {e.Exception.Message}");
+                _logger.LogError($"Ошибка: {e.Exception.Message}");
             };
 
+            // создать главное окно и запустить
+            _host.Services.GetRequiredService<MainWindow>().Show();
 
-            base.OnStartup(e);
         }
 
-        protected override void OnExit(ExitEventArgs e)
+        protected override async void OnExit(ExitEventArgs e)
         {
             _logger.LogInformation("Завершение приложения");
             AppService.OnExit();
+            await _host.StopAsync();
             base.OnExit(e);
+        }
+
+        private async Task CreateHost()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .UseSerilog((_, config) =>
+                {
+                    config
+                        .MinimumLevel.Verbose()
+                        .Enrich.WithProperty("MachineName", Environment.MachineName)
+                        .WriteTo.File(
+                            path: $@"Logs\{System.Diagnostics.Process.GetCurrentProcess().ProcessName}_.log",
+                            outputTemplate:
+                            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{MachineName}] {Message:lj}{NewLine}{Exception}",
+                            rollingInterval: RollingInterval.Month,
+                            retainedFileCountLimit: 6);
+                })
+                .ConfigureServices((_, services) =>
+                {
+                    ConfigureServices(services);
+                })
+                .Build();
+
+            await _host.StartAsync();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // === логирование ===
+            services.AddSingleton(typeof(ILoggingService<>), typeof(LoggingService<>));
+
+            // === Memory Log ===
+            services.AddSingleton<IFilterMemoryLog, FilterMemoryLog>();
+            services.AddSingleton<IMemoryLogStore, MemoryLogStore>();
+            services.AddSingleton<IIncidentTracker, IncidentTracker>();
+
+            // === Watchdog ===
+            services.AddSingleton<IWatchdogFactory, WatchdogFactory>();
+            // работать с данными из БД Sqlite (Watchdogs.db)
+            services.AddSingleton<IWatchdogManager, ManageWatchdogBySqlite>();
+            // работать с данными из папки Watchdogs (XML-файлы)
+            //.AddSingleton<IWatchdogManager, ManageWatchdogByXml>()
+            services.AddTransient<Watchdog>();
+
+            // === View-model и View
+            services.AddTransient<EditWatchdogWindowFactory>();
+            services.AddSingleton<MemoryLogViewModel>();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<MainWindow>();
+
+            // === фоновые задачи ===
+            services.AddHostedService<MyBackgroundService>();
         }
     }
 }
